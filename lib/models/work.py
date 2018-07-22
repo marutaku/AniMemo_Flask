a25411253ae518429a5e8f643a2eefdfc3920b5d
sh05 <shogonakamoto0107@gmail.com>
2018-07-22 18:55:50 +0900

    WIP for merge

--- lib/models/work.py
+++ lib/models/work.py
@@ -25,3 +25,19 @@ class WorkModel():
                 image_path = work['image_path'] + image
                 self.work_db.insert_work(work['title'], image_path, work['title_short1'], work['title_short2'], work['title_short3'], work['year'], work['cours_id'], work['public_url'], work['twitter_account'], work['twitter_hash_tag'])
         return True
+
+    def search_works(self, year):
+        for cours in [1, 2, 3, 4]:
+            works = self.shangrila_api.get_by_year_and_cours(year, cours)
+            for work in works:
+                # get image_path by public_url
+                soup = BeautifulSoup(requests.get(work['public_url']).content,'html.parser')
+                link = soup.find("img")
+                image = None
+                if link.get("src").endswith(".jpg"):
+                    image = link.get("src")
+                elif link.get("src").endswith(".png"):
+                    image = link.get("src")
+                image_path = work['image_path'] + image
+                self.work_db.insert_work(work['title'], image_path, work['title_short1'], work['title_short2'], work['title_short3'], work['year'], work['cours_id'], work['public_url'], work['twitter_account'], work['twitter_hash_tag'])
+        return True
--- lib/views/work.py
+++ lib/views/work.py
@@ -3,7 +3,30 @@ from lib.models.Work import WorkModel
 from lib import login_required
 work = Blueprint('work', __name__, url_prefix='/work')
 
-@work.route('/')
+@work.route('/', methods=['GET'])
 @login_required
-def render_work():
-    return render_template('works.html')
+def render_works():
+    works_model = WorkModel()
+    if request.args.get('title') is not None and request.args.get('year') is not None and request.args.get('cours') is not None:
+        works = 1
+        return render_template('works.html', works=works)
+    if request.args.get('title') is not None and request.args.get('year') is not None:
+        works = 2
+        return render_template('works.html', works=works)
+    if request.args.get('title') is not None and request.args.get('cours') is not None:
+        works = 3
+        return render_template('works.html', works=works)
+    if request.args.get('year') is not None and request.args.get('cours') is not None:
+        works = 4
+        return render_template('works.html', works=works)
+    if request.args.get('title') is not None:
+        works = 5
+        return render_template('works.html', works=works)
+    if request.args.get('year') is not None:
+        works = 6
+        return render_template('works.html', works=works)
+    if request.args.get('cours') is not None:
+        works = 7
+        return render_template('works.html', works=works)
+    works = 9
+    return render_template('works.html', works=works)
