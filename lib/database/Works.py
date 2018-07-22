bb6cff3fe0876c61d0efc839d905481224fb1665
sh05 <shogonakamoto0107@gmail.com>
2018-07-22 18:17:23 +0900

    add searches

--- lib/database/Works.py
+++ lib/database/Works.py
@@ -8,8 +8,32 @@ class Works(AbstractDB):
         sql = 'SELECT * FROM works'
         return self._request(sql)
 
-    def get_works_by_id(self, works_id):
-        sql = 'SELECT * FROM works WHERE id = {id}'.format(id=works_id)
+    def get_works_by_title(self, title):
+        sql = 'SELECT * FROM works WHERE title like %"{title}"%'.format(title=title)
+        return self._request(sql)
+
+    def get_works_by_year(self, year):
+        sql = 'SELECT * FROM works WHERE year = {year}'.format(year=year)
+        return self._request(sql)
+
+    def get_works_by_cours(self, cours):
+        sql = 'SELECT * FROM works WHERE cours = {cours}'.format(cours=cours)
+        return self._request(sql)
+
+    def get_works_by_title_and_year(self, title, year):
+        sql = 'SELECT * FROM works WHERE like %"{title}"% AND year = {year}'.format(title=title, year=year)
+        return self._request(sql)
+
+    def get_works_by_title_and_cours(self, title, cours):
+        sql = 'SELECT * FROM works WHERE like %"{title}"% AND cours = {cours}'.format(title=title, cours=cours)
+        return self._request(sql)
+
+    def get_works_by_year_and_cours(self, year, cours):
+        sql = 'SELECT * FROM works WHERE year = {year} AND cours = {cours}'.format(year=year, cours=cours)
+        return self._request(sql)
+
+    def get_works_by_title_and_year_and_cours(self, title, year, cours):
+        sql = 'SELECT * FROM works WHERE like %"{title}"% AND year = {year} AND cours = {cours}'.format(title=title, year=year, cours=cours)
         return self._request(sql)
 
     def insert_work(self, title, image_path, title_short1, title_short2, title_short3, year, cours, public_url, twitter_account, twitter_hash_tag):
--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from lib.views.top import top
 from lib.views.user import user
-from lib.views.works import works
+from lib.views.work import work
 from lib import load_user
 
 
@@ -11,7 +11,7 @@ app.config.from_object('lib.config')
 app.before_request(load_user)
 app.register_blueprint(top)
 app.register_blueprint(user)
-app.register_blueprint(works)
+app.register_blueprint(work)
 app.secret_key = 'hogehoge'
 
 
