ac077fc99cd321df8f2e01aed6ec892580bb5487
marutaku <satouredstone@gmail.com>
2018-07-22 17:08:34 +0900

    Fix

--- lib/models/work.py
+++ lib/models/work.py
@@ -1,30 +1,23 @@
-from bs4 import BeautifulSoup 
-from lib.database.Works import Work
+from bs4 import BeautifulSoup
+from lib.database.Works import Works
 from lib.api.ShangriLa import ShangriLa
+from lib.models.Utils import Utils
+import requests
+
 
 
 class WorkModel():
-    work_db = Work()
-
-    def login(self, name, password):
-        hashed_password = Utils.hash_password(password)
-        user_dict = self.user_db.get_user(user_name=name)
-        # ユーザの存在確認
-        if len(user_dict) == 0:
-            return False
-        #パスワードの照合
-        for user in user_dict:
-            if user['password'] == hashed_password:
-                return user
-        return False
+    work_db = Works()
+    shangrila_api = ShangriLa()
 
     def store_works(self, year):
         for cours in [1, 2, 3, 4]:
-            works = get_by_year_and_cours(year, cours):
+            works = self.shangrila_api.get_by_year_and_cours(year, cours)
             for work in works:
                 # get image_path by public_url
                 soup = BeautifulSoup(requests.get(work['public_url']).content,'html.parser')
                 link = soup.find("img")
+                image = None
                 if link.get("src").endswith(".jpg"):
                     image = link.get("src")
                 elif link.get("src").endswith(".png"):
