944257c1bdc9d6a320f250dc936d2cf21d4e0bcf
sh05 <shogonakamoto0107@gmail.com>
2018-07-22 16:43:48 +0900

    add work

--- lib/database/Works.py
+++ lib/database/Works.py
@@ -8,7 +8,7 @@ class Works(AbstractDB):
         sql = 'SELECT * FROM works WHERE id = {id}'.format(id=works_id)
         return self._request(sql)
 
-    def insert_works(self, title, image_path, title_short1, title_short2, title_short3, year, cours, public_url, twitter_account, twitter_hash_tag):
+    def insert_work(self, title, image_path, title_short1, title_short2, title_short3, year, cours, public_url, twitter_account, twitter_hash_tag):
         sql = 'INSERT INTO works(title, image_path, title_short1, title_short2, title_short3, year, cours, public_url, twitter_account, twitter_hash_tag) VALUES("{title}", "{image_path}", "{title_short1}", "{title_short2}", "{title_short3}", "{year}", "{cours}", "{public_url}", "{twitter_account}", "{twitter_hash_tag}")'.format(title=title,
                                                                                                               image_path=image_path,
                                                                                                               title_short1=title_short1,
---
+++ lib/models/work.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup 
+from lib.database.Works import Work
+
+
+class WorkModel():
+    work_db = Work()
+
+    def login(self, name, password):
+        hashed_password = Utils.hash_password(password)
+        user_dict = self.user_db.get_user(user_name=name)
+        # ユーザの存在確認
+        if len(user_dict) == 0:
+            return False
+        #パスワードの照合
+        for user in user_dict:
+            if user['password'] == hashed_password:
+                return user
+        return False
+
+    def store_works(self, year):
+        for cours in [1, 2, 3, 4]:
+            api_url = 'http://api.moemoe.tokyo/anime/v1/master/{year}/{cours}'.format(year=year, cours=cours);
+            headers = {"content-type": "application/json"}
+            works = requests.get(api_url, headers=headers).json()
+            for work in works:
+                # get image_path by public_url
+                soup = BeautifulSoup(requests.get(work['public_url']).content,'html.parser')
+                link = soup.find("img")
+                if link.get("src").endswith(".jpg"):
+                    image = link.get("src")
+                elif link.get("src").endswith(".png"):
+                    image = link.get("src")
+                image_path = work['image_path'] + image
+                self.work_db.insert_work(work['title'], image_path, work['title_short1'], work['title_short2'], work['title_short3'], work['year'], work['cours_id'], work['public_url'], work['twitter_account'], work['twitter_hash_tag'])
+        return True
