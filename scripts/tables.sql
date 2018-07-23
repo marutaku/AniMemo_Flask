97a47df79b9858a986a356dfda2a7a7664f4e6fa
marutaku <satouredstone@gmail.com>
2018-07-23 23:18:05 +0900

    Update SQL

--- scripts/tables.sql
+++ scripts/tables.sql
@@ -1,16 +1,17 @@
+BEGIN;
 create table `users` (
-    `id` int unsigned not null auto_increment primary key,
+    `id` int(11) unsigned not null auto_increment primary key,
     `name` varchar(255) not null,
     `image_path` varchar(255) null,
     `email` varchar(255) not null,
     `password` varchar(255) not null,
-    `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+    `created_at` timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
-alter table `users` add unique `users_email_unique`(`email`)
+alter table `users` add unique `users_email_unique`(`email`);
 
 create table `works` (
-    `id` int unsigned not null auto_increment primary key,
+    `id` int(11) unsigned not null auto_increment primary key,
     `title` varchar(255) not null,
     `image_path` varchar(255) null,
     `title_short1` varchar(255) null,
@@ -21,32 +22,35 @@ create table `works` (
     `public_url` varchar(255) not null,
     `twitter_account` varchar(255) not null,
     `twitter_hash_tag` varchar(255) not null,
-    `created_at` timestamp null,
-    `updated_at` timestamp null
+    `created_at` timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
 create table `follows` (
-    `id` int unsigned not null auto_increment primary key,
-    `user_id` int not null,
-    `followed_user_id` int not null,
-    `created_at` timestamp null,
-    `updated_at` timestamp null
+    `id` int(11) unsigned not null auto_increment primary key,
+    `user_id` int(11) unsigned not null,
+    `followed_user_id` int(11) unsigned not null,
+    `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (followed_user_id) REFERENCES users(id)
 );
 
 create table `reviews` (
-    `id` int unsigned not null auto_increment primary key,
-    `user_id` int not null,
-    `work_id` int not null,
+    `id` int(11) unsigned not null auto_increment primary key,
+    `user_id` int(11) unsigned not null,
+    `work_id` int(11) unsigned not null,
     `text` varchar(255) not null,
-    `created_at` timestamp null,
-    `updated_at` timestamp null
+    `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (work_id) REFERENCES works(id)
 );
 
 create table `states` (
-    `id` int unsigned not null auto_increment primary key,
+    `id` int(11) unsigned not null auto_increment primary key,
     `state` int not null,
-    `user_id` int not null,
-    `work_id` int not null,
-    `created_at` timestamp null,
-    `updated_at` timestamp null
+    `user_id` int(11) unsigned not null,
+    `work_id` int(11) unsigned not null,
+    `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (work_id) REFERENCES works(id)
 );
+COMMIT;
